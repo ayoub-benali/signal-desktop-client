e77be83c8a90b00599252928a484e93ef2383460
Ayoub Benali <ayoub.benali@nugg.ad>
2016-11-10 13:51:59 +0100

    WIP startup screen

--- src/main/scala/de/m7w3/signal/Main.scala
+++ src/main/scala/de/m7w3/signal/Main.scala
@@ -3,18 +3,32 @@ package de.m7w3.signal
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
-import scalafx.scene.Scene
-import scalafx.scene.control.Label
-import scalafx.scene.layout.BorderPane
+import scalafx.scene._
+import scalafx.scene.control.{Label, Button}
+import scalafx.scene.layout.StackPane
+import javafx.scene.layout.VBox
+
+object Step1 extends VBox{
+  val hello = new Label("Hello")
+  val appRequired = new Label("You must have Signal for Android for using this client")
+  val haveApp = new Button("I have Signal for Android")
+  // haveApp.onAction
+
+  getChildren.add(hello)
+  getChildren.add(appRequired)
+  getChildren.add(haveApp)
+}
 
 object Main extends JFXApp {
 
   stage = new PrimaryStage {
-    scene = new Scene {
-      root = new BorderPane {
-        padding = Insets(25)
-        center = new Label("Hello world")
-      }
+    title = "Welcome"
+    scene = new Scene(800, 600) {
+      // content = List(button)
+      val stack = new StackPane
+      // step1.children += List()
+      stack.getChildren.add(Step1)
+      root = stack
     }
   }
 }

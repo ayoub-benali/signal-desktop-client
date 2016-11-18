1f490e4dd61cb0ee82ca543ea594e2a6c04c33fa
Ayoub Benali <ayoub.benali@nugg.ad>
2016-11-18 14:19:18 +0100

    remove un-used import

--- src/main/scala/de/m7w3/signal/LocalKeyStore.scala
+++ src/main/scala/de/m7w3/signal/LocalKeyStore.scala
@@ -2,7 +2,6 @@ package de.m7w3.signal
 
 import org.whispersystems.signalservice.api.push.TrustStore
 import java.io.InputStream
-import java.security.KeyStore
 
 case class LocalKeyStore() extends TrustStore {
   def  getKeyStoreInputStream(): InputStream = {
